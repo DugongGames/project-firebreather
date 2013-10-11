098a7ef9dc895fdc9ff0556f79d2e657bb209feb
dugong-jimmy <jblair@dugong-games.co.uk>
2013-10-11 17:12:37 +0100

    Base unity project.
    Has a camera and some basic movement controls.

---
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+    Transform myTransform;
+    Vector3 currentDirection;
+    float moveSpeed = 2;
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float zDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            myTransform.Translate(xDir, 0, zDir);
+        }
+	}
+}

43ab7a451f0b46614ed2f7582d1712ec74429950
dugong-jimmy <jblair@dugong-games.co.uk>
2014-08-18 20:07:39 +0100

    Added some animation
    Testing the 2D animation stuff from unity.

--- Unity Project/Assets/Scripts/Controls.cs
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -14,11 +14,17 @@ public class Controls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	    
-        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        if(Input.GetButton("Vertical"))
         {
-            float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float xDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
             float zDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
             myTransform.Translate(xDir, 0, zDir);
+        }
+        if (Input.GetButton("Horizontal"))
+        {
+            float zDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            myTransform.Translate(xDir, 0, -zDir);
         }
 	}
 }

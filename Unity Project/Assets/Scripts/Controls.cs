6c9a4b352713cf47c5b76b1f9022d38f4f877797
dugong-jimmy <jblair@dugong-games.co.uk>
2014-08-20 19:15:43 +0100

    More animation
    Added animations for character walking in all directions.

--- Unity Project/Assets/Scripts/Controls.cs
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -3,28 +3,61 @@ using System.Collections;
 
 public class Controls : MonoBehaviour {
 
+    Animator animator;
     Transform myTransform;
-    Vector3 currentDirection;
-    float moveSpeed = 2;
+    public Vector3 currentDirection;
+    float moveSpeed = 3;
+    bool facingRight = true;
+    
 	// Use this for initialization
 	void Start () {
         myTransform = transform;
+        animator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
-        if(Input.GetButton("Vertical"))
+        //gets the input for movement
+        float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        //uses the input to determine what direction the character is moving for animation
+        currentDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        //messiest if statement ever to determine what direction character is walking
+        if (currentDirection == new Vector3(1, 0, 0) || currentDirection == new Vector3(-1,0,0))
         {
-            float xDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-            float zDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-            myTransform.Translate(xDir, 0, zDir);
+            animator.SetInteger("Direction", 0);
         }
-        if (Input.GetButton("Horizontal"))
+        else if (currentDirection == new Vector3(1, 0, 1) || currentDirection == new Vector3(-1, 0, 1))
         {
-            float zDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-            float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-            myTransform.Translate(xDir, 0, -zDir);
+            animator.SetInteger("Direction", 1);
         }
+        else if (currentDirection == new Vector3(0, 0, 1))
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (currentDirection == new Vector3(0,0,-1))
+        {
+            animator.SetInteger("Direction", 3);
+        }
+        else if (currentDirection == new Vector3(1, 0, -1) || currentDirection == new Vector3(-1, 0, -1))
+        {
+            animator.SetInteger("Direction", 4);
+        }
+
+        if (xDir > 0 && !facingRight)
+            Flip();
+        else if (xDir < 0 && facingRight)
+            Flip();
+        //moves the character relative to world space, to ignore the rotation of the sprites
+        myTransform.Translate(xDir + zDir,0,zDir - xDir, Space.World);     
 	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }

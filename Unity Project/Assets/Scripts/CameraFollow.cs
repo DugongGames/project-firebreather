9e53ac61e08925ad010ef43e70ee7692bd225ef5
dugong-jimmy <jblair@dugong-games.co.uk>
2014-08-29 15:15:16 +0100

    Added some terrain
    Added some terrain with hills, made some crappy textures. Updated the
    movement controls so the character can't go up steep hills.

---
+++ Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(target.position.x - 8.0f,target.position.y + 5.0f,target.position.z - 8.0f);
+	}
+}
--- Unity Project/Assets/Scripts/Controls.cs
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -4,53 +4,87 @@ using System.Collections;
 public class Controls : MonoBehaviour {
 
     Animator animator;
-    Transform myTransform;
-    public Vector3 currentDirection;
-    float moveSpeed = 3;
+    //Transform myTransform;
+    public Vector3 currentDirection;   
+    float moveSpeed;
     bool facingRight = true;
-    
+
+    //variables for movement with a charater controller
+    //float gravity = 20.0f;
+    Vector3 moveDirection;
+
+
 	// Use this for initialization
 	void Start () {
-        myTransform = transform;
+        //myTransform = transform;
         animator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //gets the input for movement
+        
+        //movement with a character controller
+        
+        CharacterController controller = GetComponent<CharacterController>();
+        
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        //Movement with transform
+        /*gets the input for movement
         float xDir = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zDir = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        */
         //uses the input to determine what direction the character is moving for animation
         currentDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
+        
         //messiest if statement ever to determine what direction character is walking
+
         if (currentDirection == new Vector3(1, 0, 0) || currentDirection == new Vector3(-1,0,0))
         {
             animator.SetInteger("Direction", 0);
+            animator.SetFloat("Speed", 1.0f);
+            moveSpeed = 7.0f;
         }
         else if (currentDirection == new Vector3(1, 0, 1) || currentDirection == new Vector3(-1, 0, 1))
         {
             animator.SetInteger("Direction", 1);
+            animator.SetFloat("Speed", 1.0f);
+            moveSpeed = 5.0f;
         }
         else if (currentDirection == new Vector3(0, 0, 1))
         {
             animator.SetInteger("Direction", 2);
+            animator.SetFloat("Speed", 1.0f);
+            moveSpeed = 7.0f;
         }
         else if (currentDirection == new Vector3(0,0,-1))
         {
             animator.SetInteger("Direction", 3);
+            animator.SetFloat("Speed", 1.0f);
+            moveSpeed = 7.0f;
         }
         else if (currentDirection == new Vector3(1, 0, -1) || currentDirection == new Vector3(-1, 0, -1))
         {
             animator.SetInteger("Direction", 4);
+            animator.SetFloat("Speed", 1.0f);
+            moveSpeed = 5.0f;
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
         }
 
-        if (xDir > 0 && !facingRight)
+        if (Input.GetAxisRaw("Horizontal") > 0 && !facingRight)
             Flip();
-        else if (xDir < 0 && facingRight)
+        else if (Input.GetAxisRaw("Horizontal") < 0 && facingRight)
             Flip();
         //moves the character relative to world space, to ignore the rotation of the sprites
-        myTransform.Translate(xDir + zDir,0,zDir - xDir, Space.World);     
+        //myTransform.Translate(xDir + zDir,0,zDir - xDir, Space.World);
+
+        //moves the character controller, ignoring y axis movement
+        controller.SimpleMove(moveDirection * moveSpeed);
 	}
 
     void Flip()

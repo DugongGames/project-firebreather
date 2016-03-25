d76cd419892893a1f4eea2210438158be1154172
Chris McCabe <chris.mccabe@sixdegreelabs.com>
2016-03-25 17:47:59 +0000

    Adding main menu

---
+++ Assets/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31.TransitionKit;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	public void WindTransition() {
+		var wind = new WindTransition()
+		{
+			nextScene = 1,
+			duration = 1.0f,
+			size = 0.3f
+		};
+		TransitionKit.instance.transitionWithDelegate( wind );
+	}
+
+	public void FishEyeTransition() {
+		var fishEye = new FishEyeTransition()
+		{
+			nextScene = 1,
+			duration = 1.0f,
+			size = 0.08f,
+			zoom = 10.0f,
+			colorSeparation = 3.0f
+		};
+		TransitionKit.instance.transitionWithDelegate( fishEye );
+	}
+
+	public void PixelateTransition() {
+		var pixelater = new PixelateTransition()
+		{
+			nextScene = 1,
+			duration = 1.0f
+		};
+		TransitionKit.instance.transitionWithDelegate( pixelater );
+	}
+
+	public void BackToMenu() {
+		var ripple = new RippleTransition()
+		{
+			nextScene = 0,
+			duration = 1.0f,
+			amplitude = 1500f,
+			speed = 20f
+		};
+		TransitionKit.instance.transitionWithDelegate( ripple );
+	}
+
+}

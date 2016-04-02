295a0db442f64f51090a5fd457d2720ea58604ed
dugong-jimmy <jblair@dugong-games.co.uk>
2016-04-02 17:34:08 +0100

    Added code to interact with the welp object

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -43,6 +43,7 @@ public class Controls : MonoBehaviour
         pos = 0;
 
         controller = gameObject.GetComponent<CharacterController>();
+
     }
 
 
@@ -65,13 +66,15 @@ public class Controls : MonoBehaviour
             */
             //uses the input to determine what direction the character is moving for animation
             currentDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            
+
+            ////Player presses key to interact
+            //if (Input.GetButtonDown("Interact"))
+            //{
+            //    interacting = true;
+            //}
+
             //messiest if statement ever to determine what direction character is walking
-            if (Input.GetButtonDown("Interact"))
-            {
-                interacting = true;
-            }
-
-
             // Right
             if (currentDirection == new Vector3(1, 0, 0))
             {
@@ -279,10 +282,16 @@ public class Controls : MonoBehaviour
     {
         if (interacting)
         {
-            if (GUI.Button(new Rect(100, 100, 100, 50), "Hello"))
+            if (GUI.Button(new Rect(200, 100, 100, 50), "Hello"))
             {
                 interacting = false;
             }
         }
     }
+
+    void Interact()
+    {  
+        interacting = true;
+    }
+
 }
---
+++ Assets/Scripts/NpcScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            col.gameObject.SendMessage("Interact");
+        }
+    }
+}
